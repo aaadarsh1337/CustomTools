c99705ad0ba4969cf3774eb1b3e1decd841c7214
Hasher2009 <adarshpillai2009@yahoo.com>
2021-03-16 12:04:29 +0000

    Updated Port Scanner

--- Python/Port Scanner.py
+++ Python/Port Scanner.py
@@ -44,7 +44,7 @@ print()
 
 def scan_port(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+    s.settimeout(2)
     conn = s.connect_ex((Fixed_Target, port))
     if (not conn):
         print(colored("[+] Port {}/tcp Is Open".format(port), 'green'))
