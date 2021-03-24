feeb97bfe52952e52cf39af8b34b87cedb2ebcb0
Hasher2009 <adarshpillai2009@yahoo.com>
2021-03-24 14:27:12 +0530

    Updated Host2IP

--- Python/Host2IP.py
+++ Python/Host2IP.py
@@ -5,8 +5,8 @@ Host = input("Enter Hostname: ")
 
 try:
     IP = socket.gethostbyname(Host)
-    print("IP Found: " + IP)
+    print("[+] IP Found: " + IP)
 
 except socket.gaierror:
-    print("Could Not Resolve Host")
+    print("[-] Could Not Resolve Host")
     sys.exit
