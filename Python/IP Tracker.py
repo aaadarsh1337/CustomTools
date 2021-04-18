faf5081021a499749ece52de80d7f1a1e40effb7
Hasher2009 <adarshpillai2009@yahoo.com>
2021-04-18 16:54:43 +0530

    Updated IP Tracker

--- Python/IP Tracker.py
+++ Python/IP Tracker.py
@@ -25,8 +25,8 @@ except KeyboardInterrupt:
     time.sleep(5)
 
 try:
-    URL = "http://ipapi.co/"
-    Response = urllib.request.urlopen(URL + Fixed_Target + '/json/')
+    URL = "http://ip-api.com/json/"
+    Response = urllib.request.urlopen(URL + Fixed_Target + "?fields=query,message,continent,continentCode,country,countryCode,region,regionName,city,zip,lat,lon,isp,timezone")
     Data = Response.read()
     Value = json.loads(Data)
 
@@ -40,26 +40,24 @@ except KeyboardInterrupt:
     time.sleep(5)
 
 try:
-    if 'ip' in Value:
-        print("[+] Target: " + Value['ip'] + "(" + Value['version'] + ")")
-    if 'reason' in Value:
-        print("[+] Message: " + Value['reason'])
-    if 'continent_code' in Value:
-        print("[+] Continent: " + Value['continent_code'])
-    if 'country_name' and 'country_code' in Value:
-        print("[+] Country: " + Value['country_name'] + "(" + Value['country_code'] + ")")
-    if 'region' and 'region_code' in Value:
-        print("[+] Region: " + Value['region'] + "(" + Value['region_code'] + ")")
-    if 'city' and 'postal' in Value:
-        print("[+] City: " + Value['city'] + "(" + Value['postal'] + ")")
-    if 'latitude' in Value:
-        print("[+] Latitude: " + str(Value['latitude']))
-    if 'longitude' in Value:
-        print("[+] Longitude: " + str(Value['longitude']))
-    if 'country_calling_code' in Value:
-        print("[+] Country Calling Code: " + str(Value['country_calling_code']))
-    if 'org' in Value:
-        print("[+] ISP: " + Value['org'])
+    if 'query' in Value:
+        print("[+] Target: " + Value['query'])
+    if 'message' in Value and ():
+        print("[+] Message: " + Value['message'])
+    if 'continent' and 'continentCode' in Value:
+        print("[+] Continent: " + Value['continent'] + "(" + Value['continentCode'] + ")")
+    if 'country' and 'countryCode' in Value:
+        print("[+] Country: " + Value['country'] + "(" + Value['countryCode'] + ")")
+    if 'region' and 'regionName' in Value:
+        print("[+] Region: " + Value['regionName'] + "(" + Value['region'] + ")")
+    if 'city' and 'zip' in Value:
+        print("[+] City: " + Value['city'] + "(" + Value['zip'] + ")")
+    if 'lat' in Value:
+        print("[+] Latitude: " + str(Value['lat']))
+    if 'lon' in Value:
+        print("[+] Longitude: " + str(Value['lon']))
+    if 'isp' in Value:
+        print("[+] ISP: " + Value['isp'])
     if 'timezone' in Value:
         print("[+] Time Zone: " + Value['timezone'])
     
